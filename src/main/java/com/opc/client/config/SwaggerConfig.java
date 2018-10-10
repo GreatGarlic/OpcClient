798ca68299d069d0100ff26982e1ac7330a9f1e1
liuyuan <405653510@qq.com>
2018-10-10 18:58:17 +0800

    commit

--- src/main/java/com/opc/client/config/SwaggerConfig.java
+++ src/main/java/com/opc/client/config/SwaggerConfig.java
@@ -1,26 +1,61 @@
 package com.opc.client.config;
 
+import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.base.Predicates.and;
+import static com.google.common.base.Predicates.not;
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .paths(Predicates.not(PathSelectors.regex("/error.*")))
-                .build();
+                .build()
+                .securitySchemes(newArrayList(new ApiKey("Authorization", "Authorization", "header")))
+                .securityContexts(newArrayList(securityContexts()));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return newArrayList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(securityPaths())
+                        .build()
+        );
+    }
+
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = new AuthorizationScope("global", "accessEverything");
+        return newArrayList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private Predicate<String> securityPaths() {
+        return and(regex("/api.*"), not(regex("/login")));
     }
 
     private ApiInfo apiInfo() {
